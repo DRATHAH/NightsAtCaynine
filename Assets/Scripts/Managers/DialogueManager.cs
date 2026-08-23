83f24a6592038b63dcbde30ac93d27f359d98eeb
DRATHAH <107436548+DRATHAH@users.noreply.github.com>
2026-08-23 11:32:43 -0400

    Singleton for Dialogue Manager

--- Assets/Scripts/Managers/DialogueManager.cs
+++ Assets/Scripts/Managers/DialogueManager.cs
@@ -2,6 +2,23 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+
+    #region Singleton
+    public static DialogueManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Dialogue Manager found!");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public CharacterInfo currentCharacter;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
