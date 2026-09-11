b700125b3c12134c848c9eeb92b6d2fff9a6d430
DRATHAH <107436548+DRATHAH@users.noreply.github.com>
2026-09-10 23:14:54 -0400

    Scene Switch Working

--- Assets/Scripts/Managers/EnvironmentManager.cs
+++ Assets/Scripts/Managers/EnvironmentManager.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class EnvironmentManager : MonoBehaviour
 {
+    public GameObject defaultScene;
+    public List<GameObject> scenes;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        foreach (GameObject scene in scenes)
+        {
+            scene.SetActive(false);
+        }
+        defaultScene.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeScene(string newScene)
     {
+        int index = scenes.FindIndex(area => area.name == newScene);
+        if (index != -1)
+        {
+            foreach (GameObject scene in scenes)
+            {
+                if (scene.activeSelf)
+                {
+                    scene.GetComponent<Animator>().SetTrigger("Exit");
+                }
+            }
+
+            scenes[index].SetActive(true);
+        }
         
     }
 }
